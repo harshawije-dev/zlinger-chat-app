6a438d74b9814896d6dc2d3d058d5033985fc864
Yasindu <yasindu@apextech.lk>
2023-12-01 17:13:02 +0530

    socket io implementing

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ import express from 'express';
 import { createServer } from 'node:http';
 import {fileURLToPath} from 'node:url';
 import {dirname, join} from 'node:path';
+import {Server} from 'socket.io'
 
 const PORT = 4200;
 const app = express();
 const server = createServer(app);
+const socket = new Server(server);
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
+socket.on('connection', (socket)=>{
+  console.log("socket");
+})
 
 app.get('/', (req, res) => {
   res.sendFile(join(__dirname, 'public/index.html'), (error)=> error);
